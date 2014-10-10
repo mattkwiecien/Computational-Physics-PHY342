5005f1d38693da3765f2cb087522dbb442e3ce91
Matthew Kwiecien <mkwieci2@gmail.com>
2014-10-10 18:40:02 -0500

    Update rocket_sim.m

--- c1/rocket_sim.m
+++ c1/rocket_sim.m
@@ -1,21 +1,44 @@
-function [  ] = rocket_sim( theta, v_ext, c, k, m, T, dt )
-g=9.81
-v_ext_x = v_ext*cos(theta);
-v_ext_y = v_ext*sin(theta);
+function [  ] = rocket_sim( theta, v_ext, c, k, m, T, N )
+g=9.81;
+v_ext_x = v_ext*cos(theta*(pi/180));
+v_ext_y = v_ext*sin(theta*(pi/180));
 
-options = odeset('RelTol',
+options = odeset('RelTol', 1e-3); 
+tspan=linspace(0,T,N);
 
-tspan=linespace(0,T,dt);
+xinit=[0;100;0;100];
 
+[t,x_ans] = ode45( @intfun, tspan, xinit, options );
 
-[t,x_ans]=ode45(@ddt, tspan,  )
+x = x_ans(:,1);
+xdot = x_ans(:,2);
+y = x_ans(:,3);
+ydot = x_ans(:,4);
 
-    function ddt = intfun(x,t)
-        ddt=[x(2); 
-            ( -c*( sqrt(x(2)^2 +x(4)^2)*x(2)) + (k*v_ext_x)) / (m - (k*t));
+hold on
+plot(x,y,'g');
+%plot(t,ydot,'b');
+%plot(t,xdot,'r');
+grid on
+ylim([0,1.1*max(y)])
+
+    function ddt = intfun(t,x)
+        ddt = [
+            x(2); 
+            ( -c*( sqrt(x(2)^2 + x(4)^2) * x(2) ) + (k*v_ext_x) ) / ( m - (k*t) );
             x(4);
-            ( (k*t - m)*g - c*(sqrt(x(2)^2 + x(4)^2)*x(4)) + (k*v_ext_y))/ (m-(k*t))];
+            ( (k*t - m)*g - c*(sqrt(x(2)^2 + x(4)^2)*x(4)) + (k*v_ext_y))/ (m-(k*t))
+            ];
     end
 
+%     function ddt = intfun(t,x)
+%         ddt=[
+%             x(2); 
+%             0;
+%             x(4);
+%             -g
+%             ];
+%     end
+
 end
-            
+
