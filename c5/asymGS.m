df6a38ae97483a8f1f4be7576416599f2da8c93b
matt <mkwieci2@gmail.com>
2014-11-17 15:13:53 -0600

    added final working version of c5

---
+++ c5/asymGS.m
@@ -0,0 +1,120 @@
+function [ grid ] = asymGS( n, E )
+
+% preallocation
+BC=1;
+grid=zeros(2*n,3*n);
+old_grid=ones(2*n,3*n);
+
+% creating the grid for our problem
+for i=1:n
+    for j=1:3*n
+        if j<=n || j>2*n 
+            grid(i, j) = NaN;
+            old_grid(i, j) = NaN;
+        end
+    end
+end
+
+% setting up boundary conditions
+% Each number corresponds to index of bc_vec
+%        ___1__
+%       |      |
+%      8|      |2
+%  __7__|      |__3__
+% |                  |
+%6|                  |4
+% |_________5________|
+%
+bc_vec = [1,0,.5,0,1,0,.5,0];
+
+%top mid
+grid(1:n, (n+1))=bc_vec(2);
+%horz mid
+grid(n+1, 1:n+1)=bc_vec(3);
+%bot right
+grid(n+1:2*n, 1)=bc_vec(4);
+
+%top mid
+grid(1:n, (2*n))=bc_vec(8);
+%horz mid
+grid(n+1, (2*n)-1:3*n)=bc_vec(7);
+%bot left
+grid(n+1:2*n, 3*n)=bc_vec(6);
+
+%bottom
+grid((2*n), 2:(3*n)-1)=bc_vec(5);
+%top
+grid(1,n+1:(2*n)-1) = bc_vec(1); 
+
+
+err=1;
+
+%second function of code - only outputs numerical grid result
+if nargin==2
+    
+    while err>E
+        for i=2:2*n-1
+            if i<=n+1
+                for j=(n+2):(2*n - 1)
+                    grid(i,j) = 1/4*(grid(i-1,j) + grid(i,j-1) + grid(i,j+1) + grid(i+1,j));
+                end
+            elseif i>n+1
+                for j=2:(3*n)-1
+                    grid(i,j) = 1/4*(grid(i-1,j) + grid(i,j-1) + grid(i,j+1) + grid(i+1,j));
+                end
+            end
+        end
+
+        ind1=isnan(grid);
+        ind2=isnan(old_grid);
+        grid_vals = grid(~ind1);
+        old_grid_vals = old_grid(~ind2);
+
+        err=sum(sum(abs(grid_vals-old_grid_vals)));
+        old_grid=grid;
+    end
+    
+else
+%animation of Gauss seidel solution to PDE
+    x=linspace(0,1,3*n);
+    y=linspace(0,1,2*n);
+    [X,Y] = meshgrid(x,y);
+    h=surf(X,Y,grid);
+    view(-40,65)
+    hold on
+    shading interp
+    %error parameter
+    E=BC*1e-6;
+    while err>E
+        %GS method
+        for i=2:2*n-1
+            if i<=n+1
+                for j=(n+2):(2*n - 1)
+                    grid(i,j) = 1/4*(grid(i-1,j) + grid(i,j-1) + grid(i,j+1) + grid(i+1,j));
+                end
+            elseif i>n+1
+                for j=2:(3*n)-1
+                    grid(i,j) = 1/4*(grid(i-1,j) + grid(i,j-1) + grid(i,j+1) + grid(i+1,j));
+                end
+            end
+        end
+        %find indices where matrix is NaN
+        ind1=isnan(grid);
+        ind2=isnan(old_grid);
+        %remove NaNs
+        grid_vals = grid(~ind1);
+        old_grid_vals = old_grid(~ind2);
+        %calculate sum of differences between current grid and old grid
+        err=sum(sum(abs(grid_vals-old_grid_vals)));
+        set(h,'Zdata',grid);
+        pause(.001)
+        %set current grid to old grid
+        old_grid=grid;
+    end
+
+    close;
+    sprintf('finished')
+end
+
+end
+
---
+++ c5/stoppingVal.m
@@ -0,0 +1,35 @@
+function [ stop ] = stoppingVal( N )
+
+%assuming 'truth' value at 1e-12
+s0 = 1e-14;
+best_grid = asymGS(N, s0);
+
+%setting up a parameter range
+stop = logspace(-2, -12, 20);
+err = zeros(size(stop));
+
+%calculating error at each parameter value
+for i = 1:length(stop)
+    
+    grid = asymGS(N, stop(i));
+
+    ind1=isnan(grid);
+    ind2=isnan(best_grid);
+    grid_vals = grid(~ind1);
+    old_grid_vals = best_grid(~ind2);
+
+    err(i) = sum(sum(abs(grid_vals-old_grid_vals)));
+    
+end
+
+%plot results
+figure
+loglog(stop, err, 'o', 'linewidth', 2);
+set(gca, 'fontsize', 18)
+
+xlabel('stopping paramater')
+ylabel('error')
+
+
+end
+
